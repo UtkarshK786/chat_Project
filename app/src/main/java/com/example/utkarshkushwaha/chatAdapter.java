354fc3bc04f67422269af271a902e0bb1d201dfd
UtkarshK786 <ukushwaha409@gmail.com>
2020-05-22 14:06:06 +0530

    new updates: chat bubbles added

--- app/src/main/java/com/example/utkarshkushwaha/chatAdapter.java
+++ app/src/main/java/com/example/utkarshkushwaha/chatAdapter.java
@@ -6,6 +6,7 @@ import android.view.View;
 import android.view.ViewGroup;
 import android.widget.ImageView;
 import android.widget.ListView;
+import android.widget.RelativeLayout;
 import android.widget.TextView;
 
 import androidx.annotation.NonNull;
@@ -37,12 +38,14 @@ public class chatAdapter extends RecyclerView.Adapter<chatAdapter.chatViewHolder
     @Override
     public void onBindViewHolder(@NonNull chatViewHolder holder, int position) {
            String cht=chatList.get(position);
-           if(sendersList.get(position).equals("Radhika")) {
+           if(sendersList.get(position).equals("Harry")||sendersList.get(position).equals("Harry:")) {
                holder.textView3.setText(cht);
+               holder.rel2.setVisibility(View.VISIBLE);
                holder.textView3.setVisibility(View.VISIBLE);
            }
            else{
                holder.textView4.setText(cht);
+               holder.rel1.setVisibility(View.VISIBLE);
                holder.textView4.setVisibility(View.VISIBLE);
            }
     }
@@ -56,6 +59,7 @@ public class chatAdapter extends RecyclerView.Adapter<chatAdapter.chatViewHolder
 
         ImageView imageView, imageView2;
         TextView textView3, textView4;
+        RelativeLayout rel1,rel2;
         public chatViewHolder(@NonNull View itemView) {
             super(itemView);
 
@@ -63,6 +67,8 @@ public class chatAdapter extends RecyclerView.Adapter<chatAdapter.chatViewHolder
             textView3=itemView.findViewById(R.id.textView3);
             textView4=itemView.findViewById(R.id.textView4);
             imageView2=itemView.findViewById(R.id.imageView2);
+            rel1=itemView.findViewById(R.id.rel1);
+            rel2=itemView.findViewById(R.id.rel2);
 
         }
     }
