4843ed726e7b0f2d5981ef4863f211b4b0592564
UtkarshK786 <ukushwaha409@gmail.com>
2020-05-23 21:01:30 +0530

    new updates: Added image

--- app/src/main/java/com/example/utkarshkushwaha/MainActivity.java
+++ app/src/main/java/com/example/utkarshkushwaha/MainActivity.java
@@ -13,7 +13,13 @@ import android.widget.Adapter;
 import android.widget.ArrayAdapter;
 import android.widget.ListView;
 import android.widget.RelativeLayout;
+import android.widget.Toast;
 
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.auth.AuthResult;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
 import com.google.firebase.database.DataSnapshot;
 import com.google.firebase.database.DatabaseError;
 import com.google.firebase.database.DatabaseReference;
@@ -34,6 +40,8 @@ public class MainActivity extends AppCompatActivity {
      static RecyclerView recyclerView;
      static Context mContext;
      static int i=1;
+     static List<String> imgs;
+    static int img_or_not=0;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -42,9 +50,13 @@ public class MainActivity extends AppCompatActivity {
         chats=new ArrayList<String>();
         senders=new ArrayList<String>();
         chatList=new ArrayList<String>();
+        imgs=new ArrayList<String>();
         mContext=this;
         relativeLayout=(RelativeLayout)findViewById(R.id.rel);
         recyclerView=findViewById(R.id.recyclerView);
+
+
+
         relativeLayout.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
@@ -56,7 +68,13 @@ public class MainActivity extends AppCompatActivity {
         });
         recyclerView.setHasFixedSize(true);  //experiment with this please
         recyclerView.setLayoutManager(new LinearLayoutManager(this));
+
+        adapter= new chatAdapter(mContext,chatList,senders,imgs);
+        recyclerView.setAdapter(adapter);
+
     }
+
+
 
     public void display_chats(){
 
@@ -74,11 +92,29 @@ public class MainActivity extends AppCompatActivity {
 //                          Log.i("required key:",dataSnapshot1.getKey());
                         for(DataSnapshot dataSnapshot3:dataSnapshot2.getChildren()) {
                             if(String.valueOf(i).equals(dataSnapshot2.getKey())) {
-                                Log.i("required key:", dataSnapshot2.getKey());
-                                Log.i(dataSnapshot3.getKey(), dataSnapshot3.getValue().toString());
-                                chatList.add(dataSnapshot3.getValue().toString());
-                                senders.add(dataSnapshot3.getKey());
-                                break;
+                                if(dataSnapshot3.getKey().equals("Radhika")||dataSnapshot3.getKey().equals("Harry")) {
+                                    Log.i("required key:", dataSnapshot2.getKey());
+                                    Log.i(dataSnapshot3.getKey(), dataSnapshot3.getValue().toString());
+                                    chatList.add(dataSnapshot3.getValue().toString());
+                                    senders.add(dataSnapshot3.getKey());
+                                    imgs.add("");
+
+                                    adapter.notifyDataSetChanged();
+                                    break;
+                                }
+                                else{
+
+                                    for(DataSnapshot dataSnapshot4:dataSnapshot3.getChildren()){
+                                        Log.i("Image sender",dataSnapshot4.getKey());
+                                        Log.i("image url",dataSnapshot4.getValue().toString());
+                                        chatList.add("");
+                                        senders.add(dataSnapshot4.getKey());
+                                        imgs.add(dataSnapshot4.getValue().toString());
+
+                                        adapter.notifyDataSetChanged();
+                                        break;
+                                    }
+                                }
                             }
                             //   arrayAdapter.notifyDataSetChanged();
                         }
@@ -86,10 +122,13 @@ public class MainActivity extends AppCompatActivity {
                 }
                 Log.i("chats",chatList.toString());
                 Log.i("senders",senders.toString());
+                Log.i("images",imgs.toString());
                 i++;
 
-        adapter= new chatAdapter(mContext,chatList,senders);
-        recyclerView.setAdapter(adapter);
+                adapter= new chatAdapter(mContext,chatList,senders,imgs);
+                recyclerView.setAdapter(adapter);
+
+                recyclerView.scrollToPosition(adapter.getItemCount() - 1);
             }
             @Override
             public void onCancelled(@NonNull DatabaseError databaseError) {
--- app/src/main/java/com/example/utkarshkushwaha/chatAdapter.java
+++ app/src/main/java/com/example/utkarshkushwaha/chatAdapter.java
@@ -1,63 +1,124 @@
 package com.example.utkarshkushwaha;
 
 import android.content.Context;
+import android.graphics.Bitmap;
 import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
-import android.widget.AdapterView;
 import android.widget.ImageView;
-import android.widget.ListView;
 import android.widget.RelativeLayout;
 import android.widget.TextView;
 
 import androidx.annotation.NonNull;
 import androidx.recyclerview.widget.RecyclerView;
 
-import com.google.firebase.database.DatabaseReference;
-import com.google.firebase.database.FirebaseDatabase;
+import com.google.android.gms.tasks.OnSuccessListener;
+import com.google.firebase.storage.FileDownloadTask;
+import com.google.firebase.storage.FirebaseStorage;
+import com.google.firebase.storage.StorageReference;
 
+import java.io.File;
+import java.util.ArrayList;
 import java.util.List;
+
+import static android.graphics.BitmapFactory.decodeFile;
 
 public class chatAdapter extends RecyclerView.Adapter<chatAdapter.chatViewHolder>{
 
     private Context context;
     private List<String> chatList;
     private List<String> sendersList;
+    private List<String> imgs;
+    private int img_or_not;
+    private FirebaseStorage firebaseStorage;
 
-    public void setOnClickListener(){
-
-    }
-
-    public chatAdapter(Context context,List<String> chatList, List<String> sendersList){
+    public chatAdapter(Context context, List<String> chatList, List<String> sendersList, List<String> imgs){
         this.context=context;
         this.chatList=chatList;
         this.sendersList=sendersList;
+        this.imgs=imgs;
+//        this.img_or_not=img_or_not;
     }
+
+
 
     @NonNull
     @Override
     public chatViewHolder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
-        LayoutInflater inflater=LayoutInflater.from(context);
-        View view=inflater.inflate(R.layout.chat_layout,null);
-        chatViewHolder holder=new chatViewHolder(view);
+        LayoutInflater inflater = LayoutInflater.from(context);
+        chatViewHolder holder = null;
+        View view = inflater.inflate(R.layout.chat_layout, null);
+            holder = new chatViewHolder(view);
         return holder;
     }
 
     @Override
-    public void onBindViewHolder(@NonNull chatViewHolder holder, int position) {
-           String cht=chatList.get(position);
-           if(sendersList.get(position).equals("Harry")||sendersList.get(position).equals("Harry:")) {
-               holder.textView3.setText(cht);
-               holder.rel2.setVisibility(View.VISIBLE);
-               holder.textView3.setVisibility(View.VISIBLE);
-           }
-           else{
-               holder.textView4.setText(cht);
-               holder.rel1.setVisibility(View.VISIBLE);
-               holder.textView4.setVisibility(View.VISIBLE);
-           }
+    public void onBindViewHolder(@NonNull final chatViewHolder holder, int position) {
+        String cht = chatList.get(position);
+        if (sendersList.get(position).equals("Harry")) {
+             if(chatList.get(position).equals("")){
+                 String imag=imgs.get(position);
+                 firebaseStorage = FirebaseStorage.getInstance();
+                 final StorageReference storageReference = firebaseStorage.getReferenceFromUrl("gs://blushed-chat.appspot.com/Radhika:Harry/imgs/"+imag);
+                 Log.i("storage","gs://blushed-chat.appspot.com/Radhika:Harry/imgs/"+cht);
+                 try {
+                     final File file = File.createTempFile("image", "jpg");
+                     storageReference.getFile(file).addOnSuccessListener(new OnSuccessListener<FileDownloadTask.TaskSnapshot>() {
+                         @Override
+                         public void onSuccess(FileDownloadTask.TaskSnapshot taskSnapshot) {
+                             Bitmap bitmap = decodeFile(file.getAbsolutePath());
+                             holder.imageView.setImageBitmap(bitmap);
+                             holder.textView3.setVisibility(View.GONE);
+                             holder.rel2.setVisibility(View.VISIBLE);
+
+                             Log.i("Stroage reference", bitmap.toString());
+
+                         }
+                     });
+
+                 } catch (Exception e) {
+                     e.printStackTrace();
+                 }
+             }else {
+                 holder.textView3.setText(cht);
+                 holder.rel2.setVisibility(View.VISIBLE);
+                 holder.imageView.setVisibility(View.GONE);
+             }
+
+        } else {
+            if(chatList.get(position).equals("")) {
+                String imag = imgs.get(position);
+                firebaseStorage = FirebaseStorage.getInstance();
+                final StorageReference storageReference = firebaseStorage.getReferenceFromUrl("gs://blushed-chat.appspot.com/Radhika:Harry/imgs/" + imag);
+                Log.i("storage", "gs://blushed-chat.appspot.com/Radhika:Harry/imgs/" + imag);
+                try {
+                    final File file = File.createTempFile("image", "jpg");
+                    storageReference.getFile(file).addOnSuccessListener(new OnSuccessListener<FileDownloadTask.TaskSnapshot>() {
+                        @Override
+                        public void onSuccess(FileDownloadTask.TaskSnapshot taskSnapshot) {
+                            Bitmap bitmap = decodeFile(file.getAbsolutePath());
+                            holder.imageView2.setImageBitmap(bitmap);
+                            holder.textView4.setVisibility(View.GONE);
+                            holder.rel1.setVisibility(View.VISIBLE);
+                            Log.i("Stroage reference", bitmap.toString());
+
+                        }
+                    });
+
+                } catch (Exception e) {
+                    e.printStackTrace();
+                }
+            }else{
+                    holder.textView4.setText(cht);
+                    holder.imageView2.setVisibility(View.GONE);
+                holder.rel1.setVisibility(View.VISIBLE);
+                }
+
+        }
     }
+
+
 
     @Override
     public int getItemCount() {
@@ -90,3 +151,13 @@ public class chatAdapter extends RecyclerView.Adapter<chatAdapter.chatViewHolder
         }
     }
 }
+
+//                   storageReference.getBytes(1024*1024).addOnSuccessListener(new OnSuccessListener<byte[]>() {
+//                       @Override
+//                       public void onSuccess(byte[] bytes) {
+//                           Bitmap bitmap= BitmapFactory.decodeByteArray(bytes,0,bytes.length);
+//                           holder.imageView2.setImageBitmap(bitmap);
+//                       }
+//                   });
+//                   holder.rel2.setVisibility(View.VISIBLE);
+//                   holder.imageView2.setVisibility(View.VISIBLE);
